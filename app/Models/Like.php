2d47d368a5a502e2ce9b1e2c50fd35f219872fc4
97qrsfcz97-ui <97qrsfcz97@privaterelay.appleid.com>
2026-02-27 17:31:11 +0800

    solved

--- app/Models/Like.php
+++ app/Models/Like.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Like extends Model
 {
     public $timestamps = false;
+    protected $primaryKey = ['post_id', 'user_id'];
+    public $incrementing = false;
 
     # post
     public function post()
