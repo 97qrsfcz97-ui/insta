b17db0882d2d99b6e0adb016510e0d3577ec220e
97qrsfcz97-ui <97qrsfcz97@privaterelay.appleid.com>
2026-02-27 17:36:06 +0800

    solveddd

--- app/Models/CategoryStory.php
+++ app/Models/CategoryStory.php
@@ -8,6 +8,8 @@ class CategoryStory extends Model
 {
     protected $table = 'category_story';
     protected $fillable = ['category_id', 'story_id'];
+    protected $primaryKey = ['category_id', 'story_id'];
+    public $incrementing = false;
     public $timestamps = false;
 
     #Get the category details for this record
